d4574a2b2b7c04e9267a6a255c93f291212fbcbd
Sohrab Khan <7ohrabkhan@gmail.com>
2025-03-14 16:37:32 +0500

    working on crousel.

--- src/animations/crousel/initializeSwipers.js
+++ src/animations/crousel/initializeSwipers.js
@@ -1,36 +1,32 @@
-// import 'swiper/css';
-// import 'swiper/css/pagination';
+
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 
 function initializeSwipers() {
   document.querySelectorAll(".swiper-pagination").forEach((pagination) => {
     pagination.style.position = "absolute";
-    pagination.style.top = "50%"; // Adjust as needed
+    pagination.style.top = "50%";
     pagination.style.left = "50%";
     pagination.style.transform = "translate(-50%,-50%)";
-    pagination.style.zIndex = "10"; // Ensure it appears on top
-    // pagination.style.display = "flex";
-    // pagination.style.justifyContent = "center";
-    // pagination.style.alignItems = "center";
+    pagination.style.zIndex = "10"; 
   });
 
   document.querySelectorAll(".swiper-container").forEach((container) => {
     const swiperInstance = new Swiper(container.querySelector(".purchase-swiper"), {
       loop: true,
+      
       modules: [Navigation, Pagination],
       navigation: {
-        nextEl: container.querySelector(".swiper-button-next"),
-        prevEl: container.querySelector(".swiper-button-prev"),
+        nextEl: container.querySelector(".custom-swiper-button-next"),
+        prevEl: container.querySelector(".custom-swiper-button-prev"),
+            
       },
       pagination: {
         el: container.querySelector(".swiper-pagination"),
-        clickable: true,
-        
+        clickable: false,        
       },
     });
 
-    // Custom navigation for mobile buttons
     const nextMobileBtn = container.querySelector(".swiper-next-button-mobile");
     const prevMobileBtn = container.querySelector(".swiper-prev-button-mobile");
 
